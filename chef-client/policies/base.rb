40201096c2cc0784b106ddcd85b502342e95caa6
Sean Beard <sean.beard@dimensiondata.com>
2019-03-11 14:39:05 +1300

    changed to policy files

---
+++ chef-client/policies/base.rb
@@ -0,0 +1,20 @@
+# Policyfile.rb - Describe how you want Chef to build your system.
+#
+# For more information on the Policyfile feature, visit
+# https://docs.chef.io/policyfile.html
+
+# A name that describes what the system you're building with Chef does.
+name "base"
+
+# Where to find external cookbooks:
+default_source :supermarket
+
+# Specify a custom source for a cookbook:
+cookbook 'chef-client', path: '../'
+
+# # run_list: chef-client will run these recipes in the order specified.
+run_list 'chef-client'
+
+# Override the Chef Client cookbook with the following attributes
+override['chef_client']['interval']    = '200'
+override['chef_client']['splay']       = '30'
