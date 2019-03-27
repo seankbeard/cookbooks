830104a4af74f048f27c25b63fe7a2fb4673cb5d
Sean Beard <sean.beard@dimensiondata.com>
2019-03-28 08:34:33 +1300

    changes to audit attributes

--- myaudit/attributes/default.rb
+++ myaudit/attributes/default.rb
@@ -1,10 +1,16 @@
-default['audit']['fetcher'] = 'chef-automate'
-default['audit']['reporter'] = 'chef-automate'
-default['audit']['server'] = 'ec2-13-237-222-164.ap-southeast-2.compute.amazonaws.com/api'
-default['audit']['insecure'] = true
-default['audit']['profiles'] = [
-  {
-    name: 'DevSec Linux Security Baseline',
-    compliance: 'admin/linux-baseline'
-  },
-]
+default['audit']['fetcher'] = 'chef-server-automate'
+default['audit']['reporter'] = 'chef-server-automate'
+
+case node['os']
+when 'linux'
+  default['audit']['profiles'] = [
+    {
+        name: 'Linux Baseline',
+        compliance: 'admin/linux-baseline',
+    },
+    {
+      name:       'DevSec Linux Patch Benchmark',
+      compliance: 'admin/linux-patch-baseline',
+    },
+  ]
+end
