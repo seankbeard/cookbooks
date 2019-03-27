40029177a65841bb0b51b16eaf8142623c82bada
Sean Beard <sean.beard@dimensiondata.com>
2019-03-28 10:53:25 +1300

    amendments to myaudit attrib

--- myaudit/attributes/default.rb
+++ myaudit/attributes/default.rb
@@ -1,16 +1,8 @@
-default['audit']['fetcher'] = 'chef-server'
-default['audit']['reporter'] = 'chef-server-automate'
-
-case node['os']
-when 'linux'
-  default['audit']['profiles'] = [
-    {
-        name: 'Linux Baseline',
-        compliance: 'admin/linux-baseline',
-    },
-    {
-      name:       'DevSec Linux Patch Benchmark',
-      compliance: 'admin/linux-patch-baseline',
-    },
-  ]
-end
+['audit']['reporter'] = 'chef-automate'
+['audit']['fetcher'] = 'chef-automate'
+['audit']['server'] =  'https://ec2-13-237-222-164.ap-southeast-2.compute.amazonaws.com/api'
+['audit']['token'] = '93a49a4f2482c64126f7b6015e6b0f30284287ee4054ff8807fb63d9cbd1c506'
+['audit']['profiles'].push(
+  'name': 'Linux Baseline',
+  'compliance': 'admin/linux-baseline' # in the ui for automate, this value is the identifier for the profile
+)
