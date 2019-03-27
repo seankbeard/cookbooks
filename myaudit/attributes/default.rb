68cffd9803b361557ad0cd918993ec54fa19361b
Sean Beard <sean.beard@dimensiondata.com>
2019-03-27 16:16:48 +1300

    changed to myaudit

--- myaudit/attributes/default.rb
+++ myaudit/attributes/default.rb
@@ -1,9 +1,10 @@
-default['audit']['reporter'] = 'chef-automate'
 default['audit']['fetcher'] = 'chef-automate'
-
-default['audit']['profiles'].push(
+default['audit']['reporter'] = 'chef-automate'
+default['audit']['server'] = 'ec2-13-237-222-164.ap-southeast-2.compute.amazonaws.com/api'
+default['audit']['insecure'] = true
+default['audit']['profiles'] = [
   {
-    'name': 'linux-basline',
-    'git': 'https://github.com/dev-sec/linux-baseline.git'
-  }
-)
+    name: 'DevSec Linux Security Baseline',
+    compliance: 'admin/linux-baseline'
+  },
+]
