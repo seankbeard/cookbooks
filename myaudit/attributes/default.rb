b5e17629fb0edcf3148c3e1742e3de058a087a1e
Sean Beard <sean.beard@dimensiondata.com>
2019-03-28 12:12:32 +1300

    amendments to myaudit attrib

--- myaudit/attributes/default.rb
+++ myaudit/attributes/default.rb
@@ -1,6 +1,6 @@
-default['audit']['reporter'] = 'chef-automate'
-default['audit']['fetcher'] = 'chef-automate'
-default['audit']['token'] = '93a49a4f2482c64126f7b6015e6b0f30284287ee4054ff8807fb63d9cbd1c506'
+default['audit']['reporter']='chef-automate'
+default['audit']['fetcher']='chef-automate'
+default['audit']['token']='93a49a4f2482c64126f7b6015e6b0f30284287ee4054ff8807fb63d9cbd1c506'
 default['audit']['profiles'].push(
   'name': 'Linux Baseline',
   'compliance': 'admin/linux-baseline'
