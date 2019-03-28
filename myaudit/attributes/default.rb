5394802f45c2e6b8ff6ee16d7d55f40bd0c94b6c
Sean Beard <sean.beard@dimensiondata.com>
2019-03-28 14:06:21 +1300

    amendments to myaudit attrib

--- myaudit/attributes/default.rb
+++ myaudit/attributes/default.rb
@@ -1,7 +1,7 @@
 default['audit']['reporter'] = 'chef-server-automate'
 default['audit']['profiles'].push(
   {
-    'name': 'ssh',
-    'supermarket': 'dev-sec/linux-baseline'
+    'name': 'ssl',
+    'git': 'https://github.com/dev-sec/linux-baseline.git'
   }
 )
