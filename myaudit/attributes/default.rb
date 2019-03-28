d030a0ff19154fa4e3ab8316a6c60dfc8dccfa51
Sean Beard <sean.beard@dimensiondata.com>
2019-03-28 13:27:48 +1300

    amendments to myaudit attrib

--- myaudit/attributes/default.rb
+++ myaudit/attributes/default.rb
@@ -1,10 +1,2 @@
 default['audit']['reporter'] = 'chef-server-automate'
-default['audit']['fetcher'] = 'chef-server-automate'
-default['audit']['profiles'].push(
-  'name': 'Linux Baseline',
-  'compliance': 'admin/linux-baseline'
-)
-# default['audit']['interval'] = {
-#   'enabled': true
-#   'time': 1440  # once a day, the default value
-# }
+default['audit']['profiles']['ssh'] = { 'supermarket': 'dev-sec/linux-baseline' }
