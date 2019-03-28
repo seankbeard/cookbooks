22d0d732540385c5ad19eb6b2f6a7e22d682ab9e
Sean Beard <sean.beard@dimensiondata.com>
2019-03-28 14:41:07 +1300

    amendments to myaudit attrib

--- myaudit/attributes/default.rb
+++ myaudit/attributes/default.rb
@@ -1,7 +1,33 @@
+default['audit']['fetcher'] = 'chef-server'
 default['audit']['reporter'] = 'chef-server-automate'
-default['audit']['profiles'].push(
-  {
-    'name': 'ssl',
-    'git': 'https://github.com/dev-sec/linux-baseline.git'
-  }
-)
+default['audit']['profiles'] =
+  case node['platform']
+  when 'centos'
+    default['audit']['profiles'] = [
+      {
+        name: 'DevSec Linux Security Baseline',
+        compliance: 'admin/linux-baseline',
+      },
+    ]
+  when 'ubuntu'
+    default['audit']['profiles'] = [
+      {
+        name: 'DevSec Linux Security Baseline',
+        compliance: 'admin/linux-baseline',
+      },
+    ]
+  when 'windows'
+    default['audit']['profiles'] = [
+      {
+        name: 'DevSec Windows Security Baseline',
+        compliance: 'workstation-1/windows-baseline',
+      },
+    ]
+  when 'redhat'
+    default['audit']['profiles'] = [
+      {
+        name: 'DevSec Linux Security Baseline',
+        compliance: 'admin/linux-baseline',
+      },
+    ]
+  end
