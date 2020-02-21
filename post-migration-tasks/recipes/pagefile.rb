a46b94bc3f76407864be4ada1d4862546408f3b1
Sean Beard <seanbeard@Seans-MacBook-Pro.local>
2020-02-21 14:33:15 +1300

    added change drive letter

--- post-migration-tasks/recipes/pagefile.rb
+++ post-migration-tasks/recipes/pagefile.rb
@@ -1,9 +1,14 @@
-windows_pagefile 'Delete the pagefile' do
+powershell_script 'set temp drive to T:' do
+    guard_interpreter :powershell_script
+    code 'Get-Partition -DiskNumber 1 | Set-Partition -NewDriveLetter T'
+  end
+
+windows_pagefile 'Delete the C: pagefile' do
     path 'C:\pagefile.sys'
     action :delete
 end
 
-windows_pagefile 'create the pagefile' do
-    path 'D:\pagefile.sys'
+windows_pagefile 'create the pagefile on T:' do
+    path 'T:\pagefile.sys'
     system_managed true
 end
